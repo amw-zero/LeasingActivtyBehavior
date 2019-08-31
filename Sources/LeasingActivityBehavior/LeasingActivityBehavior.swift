50e23a58162890463face88d557cb40f0612b33f
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-08-31 19:56:32 -0400

    Add LeasingActivity behavior model

--- Sources/LeasingActivityBehavior/LeasingActivityBehavior.swift
+++ Sources/LeasingActivityBehavior/LeasingActivityBehavior.swift
@@ -1,3 +1,40 @@
-struct LeasingActivityBehavior {
-    var text = "Hello, World!"
+class DealShell {
+  let repository: ServerRepository
+  var deals: [Deal] = [] {
+      didSet {
+          subscription(deals)
+      }
+  }
+  var subscription: ([Deal]) -> Void = { _ in }
+  
+  init(repository: ServerRepository) {
+      self.repository = repository
+  }
+  
+  func createDeal(requirementSize: Int) {
+      repository.createDeal(requirementSize: requirementSize) { result in
+          switch result {
+          case let .success(deal):
+              self.deals = self.deals + [deal]
+          default:
+              break
+          }
+      }
+  }
+}
+
+enum NetworkResult<T> {
+    case error
+    case success(T)
+}
+
+struct Deal: Codable {
+    let id: Int?
+    let requirementSize: Int
+}
+
+protocol ServerRepository {
+    var successfulResponse: Bool { get set }
+    
+    func createDeal(requirementSize: Int, onComplete: @escaping (NetworkResult<Deal>) -> Void)
 }
