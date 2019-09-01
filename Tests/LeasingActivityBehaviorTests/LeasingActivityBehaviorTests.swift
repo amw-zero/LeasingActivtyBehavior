95c296f27e3bb8f637a05b77fcbb09131daca1b6
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-08-31 20:05:28 -0400

    Bring over tests

--- Tests/LeasingActivityBehaviorTests/LeasingActivityBehaviorTests.swift
+++ Tests/LeasingActivityBehaviorTests/LeasingActivityBehaviorTests.swift
@@ -1,15 +1,47 @@
 import XCTest
 @testable import LeasingActivityBehavior
 
-final class LeasingActivityBehaviorTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(LeasingActivityBehavior().text, "Hello, World!")
+struct StubServerRepository: ServerRepository {
+    var successfulResponse: Bool = true
+    static var dealCount = 0
+    
+    func createDeal(requirementSize: Int, onComplete: @escaping (NetworkResult<Deal>) -> Void) {
+        if successfulResponse {
+            StubServerRepository.dealCount += 1
+            onComplete(.success(Deal(id: StubServerRepository.dealCount, requirementSize: requirementSize)))
+        } else {
+            onComplete(.error)
+        }
     }
+}
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+extension DealShell {
+    func hasDeal(id: Int) -> Bool {
+        deals.contains { $0.id == id }
+    }
+}
+
+func makeDealShell(isResponseSuccessful: Bool = true) -> DealShell {
+    var repository = StubServerRepository()
+    repository.successfulResponse = isResponseSuccessful
+    
+    return DealShell(repository: repository)
+}
+
+final class LeasingActivityBehaviorTests: XCTestCase {
+    func testCreatingADealSuccessfully() {
+        let shell = makeDealShell()
+        
+        shell.createDeal(requirementSize: 1000)
+        
+        XCTAssertTrue(shell.hasDeal(id: 1))
+    }
+    
+    func testCreatingADealError() {
+        let shell = makeDealShell(isResponseSuccessful: false)
+        
+        shell.createDeal(requirementSize: 1000)
+        
+        XCTAssertFalse(shell.hasDeal(id: 1))
+    }
 }
