83d8a1b596e1ee5f33f6fa1598a441f770c1a158
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-09-01 19:41:10 -0400

    Xcode indentation

--- Sources/LeasingActivityBehavior/LeasingActivityBehavior.swift
+++ Sources/LeasingActivityBehavior/LeasingActivityBehavior.swift
@@ -1,87 +1,87 @@
 import Foundation
 
 public class DealShell {
-  let serverLinkage: ServerLinkage
-  var deals: [Deal] = [] {
-    didSet {
-        subscription(deals)
-    }
-  }
-  public var subscription: ([Deal]) -> Void = { _ in }
-  
-  public init(serverLinkage: @escaping ServerLinkage) {
-    self.serverLinkage = serverLinkage
-  }
-  
-  public func createDeal(requirementSize: Int) {
-    do {
-      let params = [
-        "requirementSize": requirementSize
-      ]
-      let dealData = try JSONSerialization.data(withJSONObject: params, options: [])
-      serverLinkage(dealData) { responseResult in
-        switch responseResult {
-        case let .success(data):
-            do {
-              let deal = try JSONDecoder().decode(Deal.self, from: data)
-              self.deals = self.deals + [deal]
-            } catch {
-
-            }
-        default:
-            break
+    let serverLinkage: ServerLinkage
+    var deals: [Deal] = [] {
+        didSet {
+            subscription(deals)
         }
-      }
-    } catch {
-
     }
-  }
+    public var subscription: ([Deal]) -> Void = { _ in }
+    
+    public init(serverLinkage: @escaping ServerLinkage) {
+        self.serverLinkage = serverLinkage
+    }
+    
+    public func createDeal(requirementSize: Int) {
+        do {
+            let params = [
+                "requirementSize": requirementSize
+            ]
+            let dealData = try JSONSerialization.data(withJSONObject: params, options: [])
+            serverLinkage(dealData) { responseResult in
+                switch responseResult {
+                case let .success(data):
+                    do {
+                        let deal = try JSONDecoder().decode(Deal.self, from: data)
+                        self.deals = self.deals + [deal]
+                    } catch {
+                        
+                    }
+                default:
+                    break
+                }
+            }
+        } catch {
+            
+        }
+    }
 }
 
 public class DealServer {
-  public var successfulResponse: Bool = true
-  public typealias DealFunc = (Deal) -> Void
-  public typealias DealCreateRepository = (Deal, @escaping DealFunc) -> Void
-
-  public init() {
-  }
-
-  public func createDeal(data: Data, repository: DealCreateRepository, onComplete: @escaping (NetworkResult<Data>) -> Void) {
-    if !successfulResponse {
-      onComplete(.error)
-      return
+    public var successfulResponse: Bool = true
+    public typealias DealFunc = (Deal) -> Void
+    public typealias DealCreateRepository = (Deal, @escaping DealFunc) -> Void
+    
+    public init() {
     }
-
-    do {
-      let dealCreate = try JSONDecoder().decode(Deal.self, from: data)
-      repository(dealCreate) { deal in
-        do {
-          let dealData = try JSONEncoder().encode(deal)
-          onComplete(.success(dealData))
-        } catch {
-
+    
+    public func createDeal(data: Data, repository: DealCreateRepository, onComplete: @escaping (NetworkResult<Data>) -> Void) {
+        if !successfulResponse {
+            onComplete(.error)
+            return
         }
-      }
-      
-    } catch {
-      onComplete(.error)
+        
+        do {
+            let dealCreate = try JSONDecoder().decode(Deal.self, from: data)
+            repository(dealCreate) { deal in
+                do {
+                    let dealData = try JSONEncoder().encode(deal)
+                    onComplete(.success(dealData))
+                } catch {
+                    
+                }
+            }
+            
+        } catch {
+            onComplete(.error)
+        }
     }
-  }
 }
 
 public enum NetworkResult<T> {
-  case error
-  case success(T)
+    case error
+    case success(T)
 }
 
 public struct Deal: Codable {
-  public let id: Int?
-  public let requirementSize: Int
-
-  public init(id: Int?, requirementSize: Int) {
-    self.id = id
-    self.requirementSize = requirementSize
-  }
+    public let id: Int?
+    public let requirementSize: Int
+    
+    public init(id: Int?, requirementSize: Int) {
+        self.id = id
+        self.requirementSize = requirementSize
+    }
 }
 
 public typealias RequestFunc = (NetworkResult<Data>) -> Void
