a42b4f2c971cf187774348c76260e53e499f9db9
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-08-31 21:19:03 -0400

    Fix indentation

--- Sources/LeasingActivityBehavior/LeasingActivityBehavior.swift
+++ Sources/LeasingActivityBehavior/LeasingActivityBehavior.swift
@@ -1,45 +1,45 @@
 public class DealShell {
   let repository: ServerRepository
   var deals: [Deal] = [] {
-      didSet {
-          subscription(deals)
-      }
+    didSet {
+        subscription(deals)
+    }
   }
   public var subscription: ([Deal]) -> Void = { _ in }
   
   public init(repository: ServerRepository) {
-      self.repository = repository
+    self.repository = repository
   }
   
   public func createDeal(requirementSize: Int) {
-      repository.createDeal(requirementSize: requirementSize) { result in
-          switch result {
-          case let .success(deal):
-              self.deals = self.deals + [deal]
-          default:
-              break
-          }
-      }
+    repository.createDeal(requirementSize: requirementSize) { result in
+        switch result {
+        case let .success(deal):
+            self.deals = self.deals + [deal]
+        default:
+            break
+        }
+    }
   }
 }
 
 public enum NetworkResult<T> {
-    case error
-    case success(T)
+  case error
+  case success(T)
 }
 
 public struct Deal: Codable {
-    public let id: Int?
-    public let requirementSize: Int
+  public let id: Int?
+  public let requirementSize: Int
 
-    public init(id: Int?, requirementSize: Int) {
-      self.id = id
-      self.requirementSize = requirementSize
-    }
+  public init(id: Int?, requirementSize: Int) {
+    self.id = id
+    self.requirementSize = requirementSize
+  }
 }
 
 public protocol ServerRepository {
-    var successfulResponse: Bool { get set }
-    
-    func createDeal(requirementSize: Int, onComplete: @escaping (NetworkResult<Deal>) -> Void)
+  var successfulResponse: Bool { get set }
+  
+  func createDeal(requirementSize: Int, onComplete: @escaping (NetworkResult<Deal>) -> Void)
 }
