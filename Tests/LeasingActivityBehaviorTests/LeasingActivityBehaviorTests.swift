f01b807a603bcc5672fa7b37a511cd90465e5410
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-08-31 21:21:34 -0400

    Formatting

--- Tests/LeasingActivityBehaviorTests/LeasingActivityBehaviorTests.swift
+++ Tests/LeasingActivityBehaviorTests/LeasingActivityBehaviorTests.swift
@@ -2,46 +2,48 @@ import XCTest
 @testable import LeasingActivityBehavior
 
 struct StubServerRepository: ServerRepository {
-    var successfulResponse: Bool = true
-    static var dealCount = 0
-    
-    func createDeal(requirementSize: Int, onComplete: @escaping (NetworkResult<Deal>) -> Void) {
-        if successfulResponse {
-            StubServerRepository.dealCount += 1
-            onComplete(.success(Deal(id: StubServerRepository.dealCount, requirementSize: requirementSize)))
-        } else {
-            onComplete(.error)
-        }
+  var successfulResponse: Bool = true
+  static var dealCount = 0
+  
+  func createDeal(requirementSize: Int, onComplete: @escaping (NetworkResult<Deal>) -> Void) {
+    if successfulResponse {
+      StubServerRepository.dealCount += 1
+      onComplete(.success(Deal(id: StubServerRepository.dealCount, requirementSize: requirementSize)))
+    } else {
+      onComplete(.error)
+      
     }
+  }
 }
 
 extension DealShell {
-    func hasDeal(id: Int) -> Bool {
-        deals.contains { $0.id == id }
-    }
+  func hasDeal(id: Int) -> Bool {
+    deals.contains { $0.id == id }
+  }
 }
 
 func makeDealShell(isResponseSuccessful: Bool = true) -> DealShell {
-    var repository = StubServerRepository()
-    repository.successfulResponse = isResponseSuccessful
-    
-    return DealShell(repository: repository)
+
+  var repository = StubServerRepository()
+  repository.successfulResponse = isResponseSuccessful
+  
+  return DealShell(repository: repository)
 }
 
 final class LeasingActivityBehaviorTests: XCTestCase {
-    func testCreatingADealSuccessfully() {
-        let shell = makeDealShell()
-        
-        shell.createDeal(requirementSize: 1000)
-        
-        XCTAssertTrue(shell.hasDeal(id: 1))
-    }
+  func testCreatingADealSuccessfully() {
+    let shell = makeDealShell()
     
-    func testCreatingADealError() {
-        let shell = makeDealShell(isResponseSuccessful: false)
-        
-        shell.createDeal(requirementSize: 1000)
-        
-        XCTAssertFalse(shell.hasDeal(id: 1))
-    }
+    shell.createDeal(requirementSize: 1000)
+    
+    XCTAssertTrue(shell.hasDeal(id: 1))
+  }
+  
+  func testCreatingADealError() {
+    let shell = makeDealShell(isResponseSuccessful: false)
+    
+    shell.createDeal(requirementSize: 1000)
+    
+    XCTAssertFalse(shell.hasDeal(id: 1))
+  }
 }
